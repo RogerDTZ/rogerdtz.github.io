f112a4965f386de733ed47e69a966688b514c202
RogerDTZ <dtzroger@gmail.com>
2023-11-06 13:57:25 +0800

    Add pusheen icon

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
         pages: config.pages,
         tagColors: config.tagColors,
         author: config.author,
-        icon: './static/favicon.png',
+        icon: './static/pusheen-512x512.png',
       },
     },
   ],
